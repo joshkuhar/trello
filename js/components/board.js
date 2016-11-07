99bc4e490c4b82b6c26bc058d0ba3812302d53b3
Josh Kuhar <jdkuhar@gmail.com>
2016-11-07 17:48:19 -0500

    added callback to props

--- js/components/board.js
+++ js/components/board.js
@@ -2,13 +2,20 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Lists = require('./list');
 
+
+
 var Board = function(props){
+	var onAddInputChanged = function() {
+		return(console.log('I changed'));
+	}
+	var onAddSubmit = function(e){	
+		e.preventDefault();
+		return(console.log('I was clicked'));
+	}
 	return(
 		<div>	
 			{props.title}
-			<Lists title={props.lists} content='Ace'/>
-			<Lists title={props.lists} content='Deuce'/>
-			<Lists title={props.lists} content='Three'/>
+			<Lists title={props.lists} content='Ace' onAddInputChanged={onAddInputChanged} onAddSubmit={onAddSubmit}/>
 		</div>
 		);
 };
