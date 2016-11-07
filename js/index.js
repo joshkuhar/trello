847382c61f0859ddd068ce78fde21877616ea668
Josh Kuhar <jdkuhar@gmail.com>
2016-11-07 17:50:07 -0500

    added callback

--- js/index.js
+++ js/index.js
@@ -4,19 +4,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Board = require('./components/board');
 
-// var List = function(){
-// 	var list = [];
-// 	for (var x = 0; x < 2; x++) {
-// 		list.push(<Card text="I'm the text" />);
-// 	}
-// 	return(
-// 		<div>
-// 			{list}
-// 		</div>
-// 		);	
-// };
-
-
 document.addEventListener('DOMContentLoaded', function(){
 	ReactDOM.render(<Board title='Board Name' lists='List Name'/>, document.getElementById('app'));
 });	
