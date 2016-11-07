718ba30f7a8c23b9e7e7a57c5800c9fef0357510
Josh Kuhar <jdkuhar@gmail.com>
2016-11-07 17:48:53 -0500

    added callback

--- js/components/list.js
+++ js/components/list.js
@@ -2,29 +2,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Card = require('./card');
 
-// TO ITERATE AND USE PROPS FOR STRING CONCAT
-// function Card(props){
-
-// 	var list = [];
-// 	for (var i = 0; i < 3; i++){
-// 		list.push(<div>{props.text + " " + i}</div>);
-// 	}
-
-// 	return(
-// 		<div>{list}</div>
-// 		)
-// };
-
-var List = function(props){
-	return(
+var List = function(props) {
+	return (
 		<div>
 			{props.title}
 			<Card text={props.content}/>
-			<Card text={props.content}/>
-			<Card text={props.content}/>
+			<input type='text' onChange={props.onAddInputChanged} />
+			<button type='submit' onClick={props.onAddSubmit}>Submit</button>
 		</div>
 		);
 };
+
 module.exports = List;
 
 
+// console.log('I was clicked')
